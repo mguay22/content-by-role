e88806153cab224480ac9ba919493eabf25db8b0
Michael Guay <mguay22@ip0af516b0.int.uvm.edu>
2017-09-20 11:19:27 -0400

    First Commit

---
+++ includes/class-content-by-role-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://www.example.com/
+ * @since      1.0.0
+ *
+ * @package    Content_By_Role
+ * @subpackage Content_By_Role/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Content_By_Role
+ * @subpackage Content_By_Role/includes
+ * @author     Michael Guay <guay.me@gmail.com>
+ */
+class Content_By_Role_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-content-by-role-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       http://www.example.com/
+ * @since      1.0.0
+ *
+ * @package    Content_By_Role
+ * @subpackage Content_By_Role/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Content_By_Role
+ * @subpackage Content_By_Role/includes
+ * @author     Michael Guay <guay.me@gmail.com>
+ */
+class Content_By_Role_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'content-by-role',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
